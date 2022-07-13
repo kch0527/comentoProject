bda728773b415d3446ea4d83a677b5caed970ef3
chan <jhgu128@naver.com>
2022-07-13 16:49:26 +0900

    first commit

---
+++ src/main/java/com/comento/comentoproject/ComentoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.comento.comentoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComentoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComentoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comento/comentoproject/ComentoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comento.comentoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComentoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
